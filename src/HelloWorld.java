9afcd4e7630304acc7b22af609dce491329fed30
Corey Harmon <harmon.coreyd@gmail.com>
2023-12-12 09:03:20 -0600

    starting java

---
+++ src/HelloWorld.java
@@ -0,0 +1,110 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//        int id;
+//        boolean isLoggedIn;
+//        float bankAccountBalance;
+//        id = 123;
+//        isLoggedIn = false;
+//        bankAccountBalance = 1000.23F;
+//
+//        int theAnswer = 42;
+//        String question = "What is your favorite color?";
+//
+//        // Java has a notion of constants, denoted by the final keyword.
+//        final int SIDES_OF_A_DICE = 6;
+//        final String API_KEY = "b75b703d8195f6f433ca";
+//        final String GITHUB_API_BASE_URL = "https://api.github.com";
+//
+//        int myInteger = 900;
+//        long morePrecise = myInteger;
+
+        // todo Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+//        int myFavoriteNumber = 3;
+//        System.out.println(myFavoriteNumber);
+
+        // todo Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+//        String myString = "My name is Corey";
+//        System.out.println(myString);
+
+        // todo Change your code to assign a character value to myString. What do you notice?
+//        myString = "C";
+//        System.out.println(myString);
+
+        // todo Change your code to assign the value 3.14159 to myString. What happens?
+//        myString = 3.14159;
+//        System.out.println(myString);  // Cant do it this way
+
+        // todo Declare a long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+        /*long myNumber;
+        System.out.println(myNumber);  // Need to initialize first
+*/
+        // todo Change your code to assign the value 3.14 to myNumber. What do you notice?
+        /*myNumber = 3.14;
+        System.out.println(myNumber);*/  // Error - incompatible types. use Float or Double (double is just longer than float)
+
+        // todo Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+       /* myNumber =123L;
+        System.out.println(myNumber);*/
+
+        // todo Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+
+        // todo Copy and paste the following code blocks one at a time and execute them
+
+       /* int x = 5;
+        System.out.println(x++);
+        System.out.println(x);*/
+
+        // = 5, 6
+
+       /* int x = 5;
+        System.out.println(++x);
+        System.out.println(x);*/
+        // = 6, 6
+
+        // todo Try to create a variable named class. What happens?
+        // String myClass = "This is string";
+
+        // todo Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        System.out.println(o);
+//        int three = (int) o;
+//        System.out.println(three);
+
+        // todo Rewrite the following expressions using the relevant shorthand assignment operators:
+
+   /*     int x = 4;
+//        x = x + 5;
+
+        x += 5;
+        System.out.println(x);*/
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        System.out.println(y);
+
+   /*     int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+
+        System.out.println(y);*/
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
